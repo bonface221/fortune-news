29780d4155c60ba034cf5304030517e69a4f2c22
[bonface221] <[mainaboniface221@gmail.com]>
2022-05-05 14:55:01 +0300

    ft:added the app class interfaces

---
+++ app/models.py
@@ -0,0 +1,10 @@
+class News:
+    def __init__(self,title,image,description,date,url):
+        self.title = title
+        self.image = image
+        self.description= description
+        self.date = date
+        self.url= url
+
+
+
